5b9a9b4feb1904dd905235d984e2f9475c185c63
Ying Wang <wangzyn@amazon.com>
2021-01-05 17:36:11 -0500

    Update Granular access control

--- granular_access/lambda_functions/check_team_members/check_team_members.py
+++ granular_access/lambda_functions/check_team_members/check_team_members.py
@@ -9,10 +9,13 @@ from typing import Any, Callable, Dict, List, Optional
 
 sts_client = boto3.client("sts")
 account_id = sts_client.get_caller_identity()["Account"]
-qs_client = boto3.client('quicksight')
-lambda_aws_region = os.environ['AWS_REGION']
-print(lambda_aws_region)
 aws_region = 'us-east-1'
+lambda_aws_region = os.environ['AWS_REGION']
+qs_client = boto3.client('quicksight')
+qs_local_client = boto3.client('quicksight', region_name=lambda_aws_region)
+
+print(lambda_aws_region)
+
 ssm = boto3.client('ssm', region_name=lambda_aws_region)
 
 
@@ -72,12 +75,12 @@ def lambda_handler(event, context):
 
     path = os.path.join(tmpdir, local_file_name2)
     print(path)
-    dashboards = list_dashboards(account_id, aws_region)
+    dashboards = list_dashboards(account_id, lambda_aws_region)
 
     for dashboard in dashboards:
         dashboardid = dashboard['DashboardId']
 
-        response = describe_dashboard_permissions(account_id, dashboardid, aws_region)
+        response = describe_dashboard_permissions(account_id, dashboardid, lambda_aws_region)
         permissions = response['Permissions']
         for principal in permissions:
             actions = '|'.join(principal['Actions'])
@@ -88,16 +91,16 @@ def lambda_handler(event, context):
             principal = principal[-1]
 
             access.append(
-                [aws_region, 'dashboard', dashboard['Name'], dashboardid, ptype, principal, additional_info, actions])
+                [lambda_aws_region, 'dashboard', dashboard['Name'], dashboardid, ptype, principal, additional_info, actions])
 
-    datasets = list_datasets(account_id, aws_region)
+    datasets = list_datasets(account_id, lambda_aws_region)
 
     for dataset in datasets:
         if dataset['Name'] not in ['Business Review', 'People Overview', 'Sales Pipeline',
                                    'Web and Social Media Analytics']:
             datasetid = dataset['DataSetId']
 
-            response = describe_data_set_permissions(account_id, datasetid, aws_region)
+            response = describe_data_set_permissions(account_id, datasetid, lambda_aws_region)
             permissions = response['Permissions']
             for principal in permissions:
                 actions = '|'.join(principal['Actions'])
@@ -108,9 +111,9 @@ def lambda_handler(event, context):
                 principal = principal[-1]
 
                 access.append(
-                    [aws_region, 'dataset', dataset['Name'], datasetid, ptype, principal, additional_info, actions])
+                    [lambda_aws_region, 'dataset', dataset['Name'], datasetid, ptype, principal, additional_info, actions])
 
-    datasources = list_datasources(account_id, aws_region)
+    datasources = list_datasources(account_id, lambda_aws_region)
 
     for datasource in datasources:
         print(datasource)
@@ -120,7 +123,7 @@ def lambda_handler(event, context):
             if 'DataSourceParameters' in datasource:
                 print(datasourceid)
                 try:
-                    response = describe_data_source_permissions(account_id, datasourceid, aws_region)
+                    response = describe_data_source_permissions(account_id, datasourceid, lambda_aws_region)
                     print(response)
                     permissions = response['Permissions']
                     print(permissions)
@@ -132,10 +135,47 @@ def lambda_handler(event, context):
                         additional_info = principal[-2]
                         principal = principal[-1]
 
-                        access.append([aws_region, 'data_source', datasource['Name'], datasourceid, ptype, principal,
+                        access.append([lambda_aws_region, 'data_source', datasource['Name'], datasourceid, ptype, principal,
                                        additional_info, actions])
                 except Exception as e:
                     pass
+
+    analyses = list_analyses(account_id, lambda_aws_region)
+
+    for analysis in analyses:
+        if analysis['Status'] != 'DELETED':
+            analysisid = analysis['AnalysisId']
+
+            response = describe_analysis_permissions(account_id, analysisid, lambda_aws_region)
+            permissions = response['Permissions']
+            for principal in permissions:
+                actions = '|'.join(principal['Actions'])
+                principal = principal['Principal'].split("/")
+                ptype = principal[0].split(":")
+                ptype = ptype[-1]
+                additional_info = principal[-2]
+                principal = principal[-1]
+
+                access.append(
+                    [lambda_aws_region, 'analysis', analysis['Name'], analysisid, ptype, principal, additional_info, actions])
+
+    themes = list_themes(account_id, lambda_aws_region)
+    for theme in themes:
+        if theme['ThemeId'] not in ['SEASIDE', 'CLASSIC', 'MIDNIGHT']:
+            themeid = theme['ThemeId']
+            response = describe_theme_permissions(account_id, themeid, lambda_aws_region)
+            permissions = response['Permissions']
+            for principal in permissions:
+                actions = '|'.join(principal['Actions'])
+                principal = principal['Principal'].split("/")
+                ptype = principal[0].split(":")
+                ptype = ptype[-1]
+                additional_info = principal[-2]
+                principal = principal[-1]
+
+                access.append(
+                    [lambda_aws_region, 'theme', theme['Name'], themeid, ptype, principal, additional_info,
+                     actions])
 
     print(access)
     with open(path, 'w', newline='') as outfile:
@@ -237,6 +277,27 @@ def list_dashboards(
         aws_region=aws_region
     )
 
+def list_analyses(
+        account_id,
+        aws_region
+) -> List[Dict[str, Any]]:
+    return _list(
+        func_name="list_analyses",
+        attr_name="AnalysisSummaryList",
+        account_id=account_id,
+        aws_region=aws_region
+    )
+
+def list_themes(
+        account_id,
+        aws_region
+) -> List[Dict[str, Any]]:
+    return _list(
+        func_name="list_themes",
+        attr_name="ThemeSummaryList",
+        account_id=account_id,
+        aws_region=aws_region
+    )
 
 def describe_dashboard_permissions(account_id, dashboardid, aws_region):
     qs_client = boto3.client('quicksight', region_name=aws_region)
@@ -246,6 +307,21 @@ def describe_dashboard_permissions(account_id, dashboardid, aws_region):
     )
     return res
 
+def describe_analysis_permissions(account_id, aid, aws_region):
+    qs_client = boto3.client('quicksight', region_name=aws_region)
+    res = qs_client.describe_analysis_permissions(
+        AwsAccountId=account_id,
+        AnalysisId=aid
+    )
+    return res
+
+def describe_theme_permissions(account_id, aid, aws_region):
+    qs_client = boto3.client('quicksight', region_name=aws_region)
+    res = qs_client.describe_theme_permissions(
+        AwsAccountId=account_id,
+        ThemeId=aid
+    )
+    return res
 
 def list_datasets(
         account_id,
