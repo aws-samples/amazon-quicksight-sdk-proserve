0eafa89541eea30cc04c0ca60e4cc44f6daabe06
qmeixua <qmeixua@amazon.com>
2022-02-08 10:38:40 -0800

    Add client-agent into quicksight open source tools assets as code

--- Assets_as_Code/Assets_as_Code/src/supportive_functions.py
+++ Assets_as_Code/Assets_as_Code/src/supportive_functions.py
@@ -4,6 +4,23 @@ import time
 from IPython.display import JSON
 from IPython.display import IFrame
 from typing import Any, Dict, List, Optional
+import botocore
+import os
+
+def default_botocore_config() -> botocore.config.Config:
+    """Botocore configuration."""
+    retries_config: Dict[str, Union[str, int]] = {
+        "max_attempts": int(os.getenv("AWS_MAX_ATTEMPTS", "5")),
+    }
+    mode: Optional[str] = os.getenv("AWS_RETRY_MODE")
+    if mode:
+        retries_config["mode"] = mode
+    return botocore.config.Config(
+        retries=retries_config,
+        connect_timeout=10,
+        max_pool_connections=10,
+        user_agent_extra=f"qs_sdk_assets_as_code",
+    )
 
 # display json string in json format
 def display_json(doc, root='root'):
@@ -11,7 +28,7 @@ def display_json(doc, root='root'):
 
 #session construction
 def _assume_role(aws_account_number, role_name, aws_region):
-    sts_client = boto3.client('sts')
+    sts_client = boto3.client('sts', default_botocore_config())
     response = sts_client.assume_role(
         RoleArn='arn:aws:iam::' + aws_account_number + ':role/' + role_name,
         RoleSessionName='quicksight'
